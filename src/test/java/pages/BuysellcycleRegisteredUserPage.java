4337fac51e34eb291f613cc9ad78574e4381c9d4
murat <muratkoparan94@gmail.com>
2024-03-28 11:58:22 +0300

    sample2

---
+++ src/test/java/pages/BuysellcycleRegisteredUserPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class BuysellcycleRegisteredUserPage {
+
+    public BuysellcycleRegisteredUserPage(){PageFactory.initElements(Driver.getDriver(),this);}
+}
---
+++ src/test/java/stepdefinitions/buysellcycle_homepage.java
@@ -0,0 +1,14 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.BuysellcycleHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class buysellcycle_homepage {
+
+    BuysellcycleHomePage buysellcylePage=new BuysellcycleHomePage();
+
+
+}
