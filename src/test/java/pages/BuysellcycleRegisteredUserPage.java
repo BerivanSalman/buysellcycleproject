391043c81053129ebf4a92f42408c3e3507a0f21
Berivan Salman <bberivansalman@gmail.com>
2024-03-28 22:45:28 +0300

    03.28.2024/berivan

--- src/test/java/pages/BuysellcycleRegisteredUserPage.java
+++ src/test/java/pages/BuysellcycleRegisteredUserPage.java
@@ -11,4 +11,19 @@ public class BuysellcycleRegisteredUserPage {
 
     @FindBy(xpath = "(//*[text()='Dashboard'])[1]")
     public WebElement headDashboardButton;
+
+    @FindBy(xpath = "(//*[text()='Login'])[1]") public WebElement logInButton;
+    @FindBy(xpath = "//*[@class='img-fluid']") public WebElement imageSignInPage;
+    @FindBy(xpath = "//*[text()='Turn your ideas into reality..']") public WebElement signInPageText;
+    @FindBy(xpath = "//*[text()='Sign in with Email or Phone']") public WebElement signInForm;
+    @FindBy(xpath = "(//*[@name='login'])[2]") public WebElement emailPlaceholder;
+    @FindBy(xpath = "//*[@name='password']") public WebElement passwordPlaceholder;
+    @FindBy(xpath = "//*[@id='sign_in_btn']") public WebElement signInButton;
+    @FindBy(xpath = "//*[@class='checkmark mr_15']") public WebElement checkBoxRememberMe;
+    @FindBy(xpath = "//*[text()='Click Here']") public WebElement linkForgotPassword;
+    @FindBy(xpath = "//*[@placeholder='Email Address']") public WebElement forgotPasswordEmailPlaceholder;
+    @FindBy(xpath = "//*[text()='Send link']") public WebElement sendLinkButton;
+    @FindBy(xpath = "(//*[@class='text-danger'])[1]") public WebElement labelEmailRequired;
+    @FindBy(xpath = "(//*[@class='text-danger'])[3]") public WebElement labelPasswordRequired;
+    @FindBy(xpath = "//*[text()='These credentials do not match our records.']") public WebElement labelDoNotMatch;
 }
