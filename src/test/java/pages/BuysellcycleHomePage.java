47b403b68717ef1428a9ed3d549f54e591842523
reyhany <breyhanyildiz@gmail.com>
2024-03-29 08:41:29 +0100

    29.03.24/Reyhan-2

--- src/test/java/pages/BuysellcycleHomePage.java
+++ src/test/java/pages/BuysellcycleHomePage.java
@@ -3,10 +3,20 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
 import utilities.Driver;
+
+import java.util.Properties;
+
 
 public class BuysellcycleHomePage {
 
+
     public BuysellcycleHomePage(){PageFactory.initElements(Driver.getDriver(),this);}
+public void homePage() throws InterruptedException {
+    Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+
+}
+
 
 }
--- src/test/java/stepdefinitions/buysellcycle_homepage.java
+++ src/test/java/stepdefinitions/buysellcycle_homepage.java
@@ -2,13 +2,32 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
 import pages.BuysellcycleHomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
+import java.util.concurrent.TimeUnit;
+
 public class buysellcycle_homepage {
 
     BuysellcycleHomePage buysellcylePage=new BuysellcycleHomePage();
+
+
+
+    private WebDriver driver;
+    @Given("User goes to {string}")
+    public void user_goes_to(String url) throws InterruptedException {
+        buysellcylePage.homePage();
+    }
+
+
+
+    @Then("User closed to page")
+    public void user_closed_to_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 
 }
